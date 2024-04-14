d66c68058556b631ba79c7b5dcc9dd90fb381606
BrunoOdella <brunoodella@gmail.com>
2024-04-14 19:24:58 -0300

    Creation of Models In and Our project and folder, with classes for request and response for CreateAdmin

---
+++ BuildingManagement/Models/In/CreateAdminRequest.cs
@@ -0,0 +1,21 @@
+﻿namespace Models.In
+{
+    public class CreateAdminRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Admin ToEntity()
+        {
+            return new Admin
+            {
+                FirstName = FirstName,
+                LastName = LastName,
+                Email = Email,
+                Password = Password
+            };
+        }
+    }
+}
---
+++ BuildingManagement/Models/Out/CreateAdminResponse.cs
@@ -0,0 +1,26 @@
+﻿namespace Models.Out
+{
+    public class CreateAdminResponse
+    {
+        public int AdminId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public CreateAdminResponse(Admin admin)
+        {
+            this.AdminId = admin.AdminID;
+            this.FirstName = admin.FirstName;
+            this.LastName = admin.LastName;
+            this.Email = admin.Email;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is CreateAdminResponse response &&
+                   AdminId == response.AdminId &&
+                   FirstName == response.FirstName &&
+                   LastName == response.LastName &&
+                   Email == response.Email;
+        }
+    }
+}
