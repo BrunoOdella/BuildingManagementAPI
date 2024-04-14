f840a70bab1a1f9c93b61c0a9d2652de2f429fcb
BrunoOdella <brunoodella@gmail.com>
2024-04-14 19:19:45 -0300

    RED PostAdmin_ShouldReturnOkResponse_WhenAdminIsSuccessfullyCreated, creation of AdminControllers class with http POST CreateAdmin

---
+++ BuildingManagement/BuildingManagementApi/Controllers/AdminsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace BuildingManagementApi.Controllers
+{
+    [Route("api/admins")]
+    [ApiController]
+    public class AdminsController : ControllerBase
+    {
+        private readonly IAdminLogic _adminLogic;
+
+        public AdminsController(IAdminLogic adminLogic)
+        {
+            _adminLogic = adminLogic;
+        }
+
+        [HttpPost]
+        public IActionResult CreateAdmin([FromBody] CreateAdminRequest admin)
+        {
+            CreateAdminResponse response = new CreateAdminResponse(_adminLogic.CreateAdmin(admin.ToEntity()));
+            return Ok(response);
+        }
+    }
+}
