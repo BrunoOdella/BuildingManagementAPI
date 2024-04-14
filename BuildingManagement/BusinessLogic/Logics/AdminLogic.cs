d8392a0181eda99313a7e74d16c17ae4738ea250
BrunoOdella <brunoodella@gmail.com>
2024-04-14 19:32:27 -0300

    Creation of BL project, with Logics folder and AdminLogic class

---
+++ BuildingManagement/BusinessLogic/Logics/AdminLogic.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using LogicInterface.Interfaces;
+
+
+namespace BusinessLogic.Logics
+{
+    public class AdminLogic : IAdminLogic
+    {
+        public Admin CreateAdmin(Admin admin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
