427b30672f0aa44ef3407766a79e8d259ead5245
BrunoOdella <brunoodella@gmail.com>
2024-04-26 12:42:12 -0300

    Creation of InvitationsControllerTest TestInitialize

---
+++ BuildingManagement/BuildingManagementApiTest/InvitationsControllerTest.cs
@@ -0,0 +1,24 @@
+﻿using BuildingManagementApi.Controllers;
+using Domain;
+using LogicInterface.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Models.In;
+using Models.Out;
+using Moq;
+
+namespace BuildingManagementApiTest
+{
+    [TestClass]
+    public class InvitationsControllerTest
+    {
+        private Mock<IInvitationLogic> _InvitationsLogicMock;
+        private InvitationsController _invitationsController;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            _invitationLogicMock = new Mock<IInvitationLogic>(MockBehavior.Strict);
+            _invitationsController = new AdminsController(_invitationLogicMock.Object);
+        }
+    }
+}
