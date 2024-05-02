5a3320bd842593c3d2363fe0eb2e5eaec371ddf0
BrunoOdella <brunoodella@gmail.com>
2024-05-01 22:07:52 -0300

    add some properties to manteinance staff

--- BuildingManagement/Domain/MaintenanceStaff.cs
+++ BuildingManagement/Domain/MaintenanceStaff.cs
@@ -4,5 +4,8 @@ public class MaintenanceStaff
 {
     public Guid ID { get; set; } = Guid.NewGuid();
     public string Name { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
     public List<Request_> Requests { get; set; } = new List<Request_>();
 }
