3bf93941f1f8aefea5174d219ff4f9475249903b
BrunoOdella <brunoodella@gmail.com>
2024-05-22 14:05:28 -0300

    ConstructionCompanyLogicTest green

--- BuildingManagement/BusinessLogic/Logics/ConstructionCompanyLogic.cs
+++ BuildingManagement/BusinessLogic/Logics/ConstructionCompanyLogic.cs
@@ -1,13 +1,27 @@
-﻿using LogicInterface.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Domain;
+using IDataAccess;
+using LogicInterface.Interfaces;
+using CustomExceptions.ConstructionCompanyExceptions;
 
 namespace BusinessLogic.Logics
 {
     public class ConstructionCompanyLogic : IConstructionCompanyLogic
     {
+        private readonly IConstructionCompanyRepository _constructionCompanyRepository;
+
+        public ConstructionCompanyLogic(IConstructionCompanyRepository constructionCompanyRepository)
+        {
+            _constructionCompanyRepository = constructionCompanyRepository;
+        }
+
+        public ConstructionCompany CreateConstructionCompany(ConstructionCompany constructionCompany)
+        {
+            if (_constructionCompanyRepository.NameExists(constructionCompany.Name))
+            {
+                throw new ConstructionCompanyAlreadyExistsException();
+            }
+            
+            return _constructionCompanyRepository.CreateConstructionCompany(constructionCompany);
+        }
     }
 }
