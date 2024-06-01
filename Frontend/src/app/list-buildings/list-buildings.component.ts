603fc2eef121a315ac6694fc8ab818c45beb37b5
BrunoOdella <brunoodella@gmail.com>
2024-05-31 21:52:16 -0300

    Added features to assign manager to a buildings and manager can see his buildings and apartments

--- Frontend/src/app/list-buildings/list-buildings.component.ts
+++ Frontend/src/app/list-buildings/list-buildings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 
 interface BuildingResponse {
   buildingId: string;
@@ -10,7 +11,12 @@ interface BuildingResponse {
   longitude: number;
   constructionCompanyName: string;
   commonExpenses: number;
-  managerName: string | null;
+  managerName: string;
+}
+
+interface ManagerResponse {
+  managerId: string;
+  email: string;
 }
 
 @Component({
@@ -20,13 +26,53 @@ interface BuildingResponse {
 })
 export class ListBuildingsComponent implements OnInit {
   buildings: BuildingResponse[] = [];
+  managers: ManagerResponse[] = [];
+  managerForms: { [buildingId: string]: FormGroup } = {};
 
-  constructor(private http: HttpClient, private router: Router) {}
+  constructor(private http: HttpClient, private router: Router, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.http.get<BuildingResponse[]>('http://localhost:5154/api/v2/buildings').subscribe(
-      (data) => this.buildings = data,
+      (data) => {
+        this.buildings = data;
+        this.buildings.forEach(building => {
+          if (building.managerName === 'No Manager Assigned') {
+            this.managerForms[building.buildingId] = this.fb.group({
+              manager: new FormControl('')
+            });
+          }
+        });
+      },
       (error) => console.error(error)
+    );
+
+    this.http.get<ManagerResponse[]>('http://localhost:5154/api/v2/managers').subscribe(
+      (data) => {
+        this.managers = data;
+        console.log('Managers:', this.managers);
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  updateBuilding(building: BuildingResponse): void {
+    const selectedManagerId = this.managerForms[building.buildingId].get('manager')?.value;
+
+    const updateRequest = {
+      name: building.name,
+      address: building.address,
+      latitude: building.latitude,
+      longitude: building.longitude,
+      commonExpenses: building.commonExpenses,
+      managerGuid: selectedManagerId
+    };
+
+    this.http.put(`http://localhost:5154/api/v2/buildings/${building.buildingId}`, updateRequest).subscribe(
+      response => {
+        console.log('Building updated successfully');
+        building.managerName = this.managers.find(manager => manager.managerId === selectedManagerId)?.email || 'Assigned';
+      },
+      error => console.error(error)
     );
   }
 
